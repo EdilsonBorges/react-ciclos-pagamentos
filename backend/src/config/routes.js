88e64a176e067828d9d5f5631f0f47dd2b88164f
Edilson Borges <edilsonfb@gmail.com>
2018-02-27 23:49:51 -0300

    routes for money app

---
+++ backend/src/config/routes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+module.exports = function(server) {
+
+	// Definir URL base para todas as rotas
+	const router = express.Router()
+	server.use('/api', router)
+}
