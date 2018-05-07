d6239de580cbcbd1de35427b1ad6ed3b5c871885
Edilson Borges <edilsonfb@gmail.com>
2018-05-06 23:56:05 -0300

    agg pipeline

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -14,7 +14,14 @@ BillingCycle.route('count', (req, res, next) => {
 });
 
 BillingCycle.route('summary', (req, res, next) => {
-	res.send('ok');
+	BillingCycle.aggregate({
+		$project: {credit: {$sum: "$credits.value"}, $debts: {$sum: "$debts,value"}
+		}, {
+			$group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}
+		}, {
+			$project: {_id:0, credit:1, debt: 1 }
+		}
+	});
 });
 
 module.exports = BillingCycle;
